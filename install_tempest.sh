84bd3a4a5324339d5492fcee0b046a1d2ebf8837
Oleksii Butenko <apbutenko@gmail.com>
2018-01-15 17:54:47 +0200

    Update install_tempest.sh

--- install_tempest.sh
+++ install_tempest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xe
 pip install tempest==17.2.0
 source /home/rally/keystonercv3
-rally-manage db recreate
+rally db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest \
     --name tempest-verifier \
     --version 17.2.0 \
     --system-wide
-rally verify configure-verifier --extend /home/rally/tempest_conf
+rally verify configure-verifier --extend /home/rally/tempest.conf
 rally verify configure-verifier --show
 git clone https://github.com/openstack/heat-tempest-plugin
 pip intall -r /home/rally/heat-tempest-plugin/test-requirements.txt
