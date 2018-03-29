ea64a955aa4d3814bf0730b72b5629cd105ada7f
root <root@gtw01.virtual-mcp-pike-ovs-ceph.local>
2018-03-29 11:15:35 +0000

    fix

--- install_tempest.sh
+++ install_tempest.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -xe
-pip install tempest==17.2.0
+pip install tempest==16.0.0
 source /home/rally/keystonercv3
 rally db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest \
     --name tempest-verifier \
-    --version 17.2.0 \
+    --version 16.0.0 \
     --system-wide
 rally verify configure-verifier --extend /home/rally/tempest.conf
 rally verify configure-verifier --show
-git clone https://github.com/openstack/heat-tempest-plugin
-pip install -r /home/rally/heat-tempest-plugin/test-requirements.txt
-rally verify add-verifier-ext --source /home/rally/heat-tempest-plugin
